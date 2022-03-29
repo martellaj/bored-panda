f97d865748903b68103171ccfbaed362bc432f9f
Joe Martella <martellaj@outlook.com>
2022-03-29 00:21:46 -0700

    almost

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Icon } from "semantic-ui-react";
 import HowToPlay from "./HowToPlay";
 import About from "./About";
 import toggleDarkMode from "./toggleDarkMode";
-import { consolidate } from "./resultsCache";
+import { getStats } from "./resultsCache";
 import ScoreDistribution from "./ScoreDistribution";
 
 // set the app height for mobile
@@ -19,11 +19,27 @@ window.addEventListener("resize", appHeight);
 appHeight();
 
 // consolidate scores for results
-consolidate();
+const stats = getStats();
+
+// go to new site
+// window.location.href = `https://playcladder.com${
+//   stats ? `?stats=${JSON.stringify(stats)}` : ""
+// }`;
 
 const params = new Proxy(new URLSearchParams(window.location.search), {
   get: (searchParams, prop) => searchParams.get(prop),
 });
+
+setTimeout(() => {
+  if (params?.fwd) {
+    console.log("a");
+    // go to new site
+    window.location.href = `https://playcladder.com${
+      stats ? `?stats=${JSON.stringify(stats)}` : ""
+    }`;
+  }
+  console.log("b");
+}, 50);
 
 function App() {
   const [view, setView] = useState("menu");
--- src/resultsCache.js
+++ src/resultsCache.js
@@ -1,7 +1,13 @@
-export function consolidate() {
+export function getStats() {
   try {
-    if (!getScores()) {
+    const stats = window.localStorage.getItem("stats");
+
+    if (!stats) {
       const scores = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+      let played = 0;
+      let won = 0;
+      let totalTimeWinning = 0;
+      let fastestTime = 60;
 
       for (let i = 0; i < 10; i++) {
         const dailyResult = window.localStorage.getItem(`puzzle-${i}`);
@@ -10,13 +16,37 @@ export function consolidate() {
           const result = JSON.parse(dailyResult);
           const score = result.score;
           scores[score] = scores[score] + 1;
+
+          played++;
+          won = score === 10 ? won + 1 : won;
+          totalTimeWinning =
+            score === 10 ? totalTimeWinning + result.time : totalTimeWinning;
+          fastestTime =
+            score === 10
+              ? result.time < fastestTime
+                ? result.time
+                : fastestTime
+              : fastestTime;
         }
       }
 
-      window.localStorage.setItem("alltimeResults", scores);
+      const data = {
+        scores,
+        gamesPlayed: played,
+        gamesWon: won,
+        averageWinTime: parseFloat((totalTimeWinning / won).toFixed(2)),
+        fastestWinTime: fastestTime,
+      };
+
+      window.localStorage.setItem("stats", JSON.stringify(data));
+
+      return data;
+    } else {
+      return JSON.parse(stats);
     }
   } catch (e) {
     console.error(e);
+    return null;
   }
 }
 
